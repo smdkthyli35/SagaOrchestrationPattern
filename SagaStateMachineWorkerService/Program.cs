2599ae79e5a909501b0ff323c86bb8577f208a08
Samed Kütahyalı <smdkthyli@gmail.com>
2023-06-18 17:12:26 +0300

    OrderStateMachine has been added and Program.cs has been rearranged.

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,8 +1,39 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.Models;
+using Shared;
+using System.Reflection;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(opt =>
+            {
+                opt.AddDbContext<DbContext, OrderStateDbContext>((provider, builder) =>
+                {
+                    builder.UseSqlServer(hostContext.Configuration.GetConnectionString("SqlCon"), m =>
+                    {
+                        m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                    });
+                });
+            });
+
+            cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>
+            {
+                configure.Host(hostContext.Configuration.GetConnectionString("RabbitMQ"));
+
+                configure.ReceiveEndpoint(RabbitMQSettingsConst.OrderSaga, e =>
+                {
+                    e.ConfigureSaga<OrderStateInstance>(provider);
+                });
+            }));
+        });
+
+
+
         services.AddHostedService<Worker>();
     })
     .Build();
