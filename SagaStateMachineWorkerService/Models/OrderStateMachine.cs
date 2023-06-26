e0a548bd21b7124c9223d470e5c98afe27fd8f2e
Samed Kütahyalı <smdkthyli@gmail.com>
2023-06-26 15:37:05 +0300

    StockReservedEvent has been created.

--- SagaStateMachineWorkerService/Models/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Models/OrderStateMachine.cs
@@ -1,5 +1,6 @@
 ﻿using MassTransit;
 using Shared;
+using Shared.Events;
 using Shared.Interfaces;
 
 namespace SagaStateMachineWorkerService.Models
@@ -7,7 +8,10 @@ namespace SagaStateMachineWorkerService.Models
     public class OrderStateMachine : MassTransitStateMachine<OrderStateInstance>
     {
         public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }
+        public Event<IStockReservedEvent> StockReservedEvent { get; set; }
+
         public State OrderCreated { get; private set; }  //Bu event geldiğinde state -> Order Created olacak
+        public State StockReserved { get; private set; }
 
         public OrderStateMachine()
         {
@@ -31,6 +35,23 @@ namespace SagaStateMachineWorkerService.Models
             .Publish(context => new OrderCreatedEvent(context.Instance.CorrelationId) { OrderItems = context.Data.OrderItems })
             .TransitionTo(OrderCreated)
             .Then(context => { Console.WriteLine($"OrderCreatedRequestEvent after : {context.Instance}"); }));
+
+
+            During(OrderCreated,
+                When(StockReservedEvent)
+                .TransitionTo(StockReserved)
+                .Send(new Uri($"queue:{RabbitMQSettingsConst.PaymentStockReservedRequestQueueName}"), context => new StockReservedRequestPayment(context.Instance.CorrelationId)
+                {
+                    OrderItems = context.Data.OrderItems,
+                    Payment = new PaymentMessage()
+                    {
+                        CardName = context.Instance.CardName,
+                        CardNumber = context.Instance.CardNumber,
+                        CVV = context.Instance.CVV,
+                        Expiration = context.Instance.Expiration,
+                        TotalPrice = context.Instance.TotalPrice
+                    }
+                }).Then(context => { Console.WriteLine($"StockReservedEvent after : {context.Instance}"); }));
         }
     }
 }
